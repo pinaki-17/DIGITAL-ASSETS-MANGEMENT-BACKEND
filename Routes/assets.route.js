a75a7702de7294d4e4978ecb9addf2392d284524
pinaki-17 <pinakimitr17@gmail.com>
2025-05-30 09:01:57 +0530

    i make changes in model and controller that bind to one assetsId

--- Routes/assets.route.js
+++ Routes/assets.route.js
@@ -4,9 +4,13 @@ const router = express.Router();
 
 const AssetsController = require('../Controllers/assets.controller');
 
-router.post('/assets/createAsset',AssetsController.createAsset);
+router.post('/assets/createAsset', AssetsController.createAsset);
 router.get('/assets/:assetsId', AssetsController.getAsset);
-router.get('/assets/delete/:assetId',AssetsController.deleteAsset);
+router.delete('/assets/:assetsId', AssetsController.deleteAsset);
+router.put('/assets/update/bp/:assetsId', AssetsController.updateBP);
+router.put('/assets/update/sa/:assetsId', AssetsController.updateSA);
+router.put('/assets/update/infra/:assetsId', AssetsController.updateInfra);
+router.put('/assets/update/ts/:assetsId', AssetsController.updateTS);
 module.exports = router;
 
 
